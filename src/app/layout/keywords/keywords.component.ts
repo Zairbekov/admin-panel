92bf10e8ecfb848796de01a618228ff69484849e
Ularbek Zairbekov <ularbek.zairbekov@iaau.edu.kg>
2018-07-11 08:24:01 -0300

    first commit

---
+++ src/app/layout/keywords/keywords.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import { routerTransition } from '../../router.animations';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+    selector: 'app-tables',
+    templateUrl: './tables.component.html',
+    styleUrls: ['./tables.component.scss'],
+    animations: [routerTransition()]
+})
+export class TablesComponent implements OnInit {
+    constructor(public http: HttpClient, public modalService: NgbModal) {}
+    seconds: any;
+    closeResult: string;
+    ngOnInit() {
+  const headers = new HttpHeaders();
+    headers.append('Access-Control-Allow-Origin' , 'http://localhost:8080');
+    headers.append('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT');
+    headers.append('Accept', 'application/json');
+    headers.append('Content-type', 'charset=UTF-8');
+  this.http.get('http://localhost:8080/all', {headers: headers})
+      .subscribe(res => console.log(res));
+    }
+    toggleSeconds() {
+        this.seconds = !this.seconds;
+        console.log(this.seconds);
+    }
+     open(content) {
+        this.modalService.open(content).result.then((result) => {
+            this.closeResult = `Closed with: ${result}`;
+        }, (reason) => {
+            this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+        });
+    }
+
+    private getDismissReason(reason: any): string {
+        if (reason === ModalDismissReasons.ESC) {
+            return 'by pressing ESC';
+        } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+            return 'by clicking on a backdrop';
+        } else {
+            return  `with: ${reason}`;
+        }
+    }
+
+}
---
+++ src/app/layout/keywords/keywords.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { TablesRoutingModule } from './tables-routing.module';
+import { TablesComponent } from './tables.component';
+import { PageHeaderModule } from './../../shared';
+import {HttpModule} from '@angular/http';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+
+@NgModule({
+    imports: [CommonModule, TablesRoutingModule, PageHeaderModule, HttpModule, NgbModule.forRoot()],
+    declarations: [TablesComponent]
+
+})
+export class TablesModule {}
